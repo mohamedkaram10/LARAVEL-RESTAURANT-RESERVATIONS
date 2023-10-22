0ccef90623eac29546cf54c877ecc4e067079848
moamedkaram33 <mohammad@gridsapps.com>
2023-10-22 22:55:17 +0300

    Add Theme For Admin

--- routes/web.php
+++ routes/web.php
@@ -20,7 +20,7 @@ Route::get('/', function () {
 });
 
 Route::get('/dashboard', function () {
-    return view('admin.index');
+    return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
