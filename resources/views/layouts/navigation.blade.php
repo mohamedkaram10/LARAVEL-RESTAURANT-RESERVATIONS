ade3df97b88a84beb436608a60f0cf256827350b
moamedkaram33 <mohammad@gridsapps.com>
2023-10-23 00:36:45 +0300

    Adding CRUD For All Moduls Pages

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -12,11 +12,11 @@
 
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
-                    <x-nav-link  :active="request()->routeIs('dashboard')">
+                    <x-nav-link :href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
                     </x-nav-link>
                     @if (Auth::user()->is_admin)
-                    <x-nav-link  href="{{ route('admin.index') }}" :active="request()->routeIs('admin.index')">
+                    <x-nav-link  :href="{{ route('admin.index') }}" :active="request()->routeIs('admin.index')">
                         {{ __('Admin') }}
                     </x-nav-link>
                     @endif
