98e2705e5b1e105092194ebef6d520dec7c20ae0
moamedkaram33 <mohammad@gridsapps.com>
2023-10-22 22:06:23 +0300

    init Project

---
+++ database/seeders/Admin.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Support\Str;
+use Illuminate\Database\Seeder;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+class Admin extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::create([
+            'name' => 'Admin',
+            'email' => 'admin@gmail.com',
+            'password' => bcrypt('123456'), // password
+            'remember_token' => Str::random(10),
+            'is_admin' => 1
+        ]);
+    }
+}
